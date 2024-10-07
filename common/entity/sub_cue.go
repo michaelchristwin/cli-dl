15eb1447cc81e24f6fba2ffbe3cf9f538e397ad1
michaelchristwin <kelechukwuchristwin@gmail.com>
2024-10-07 07:49:01 +0100

    Modified hashing function

--- common/entity/sub_cue.go
+++ common/entity/sub_cue.go
@@ -1,6 +1,8 @@
 package entity
 
 import (
+	"hash"
+	"hash/fnv"
 	"time"
 
 	"github.com/michaelchristwin/N_M3U8DL-RE-go.git/common/utils"
@@ -30,4 +32,22 @@ func (s *SubCue) isEquals(other *SubCue) bool {
 	}
 
 	return true
+}
+
+func (s *SubCue) GetHashCode() int {
+	h := fnv.New64a()
+	localHashInt64(h, int64(s.StartTime))
+	localHashInt64(h, int64(s.EndTime))
+
+	hashString(h, s.Settings)
+	hashString(h, s.Payload)
+	return int(h.Sum64())
+}
+
+func localHashInt64(h hash.Hash64, i int64) {
+	var buf [8]byte
+	for j := 0; j < 8; j++ {
+		buf[j] = byte(i >> (56 - j*8))
+	}
+	h.Write(buf[:])
 }
