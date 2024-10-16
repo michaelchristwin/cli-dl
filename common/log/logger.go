2cccf4cc02ab7c3c744c1d40691b28a75d5e0ab2
michaelchristwin <kelechukwuchristwin@gmail.com>
2024-10-16 09:48:19 +0100

    Added replace variables method

--- common/log/logger.go
+++ common/log/logger.go
@@ -2,9 +2,11 @@ package log
 
 import (
 	"fmt"
+	"html"
 	"os"
 	"path/filepath"
 	"regexp"
+	"sync"
 	"time"
 )
 
@@ -13,6 +15,7 @@ type Logger struct {
 	IsWriteFile  bool
 	LogFilePath  *string
 	VarsRepRegex *regexp.Regexp
+	LogWriteLock sync.RWMutex
 }
 
 func (l *Logger) InitLogFile() {
@@ -65,11 +68,56 @@ func (l *Logger) GetCurrTime() string {
 }
 
 func (l *Logger) HandleLog(write string, subWrite string) {
+	console := NewCustomAnsiConsole(true, false)
+
 	if write == "" {
+		console.MarkupLine(write)
+	} else {
+		console.MarkupLine(write)
+		fmt.Println(subWrite)
+	}
+	if l.IsWriteFile && fileExists(l.LogFilePath) {
+		plain := fmt.Sprintf("%s%s", html.EscapeString(write), html.EscapeString(subWrite))
+		l.LogWriteLock.Lock()
+		defer l.LogWriteLock.Unlock()
+		// Open the file in append mode. If it doesn't exist, create it with 0644 permissions.
+		file, err := os.OpenFile(*l.LogFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Println("Error opening or creating file:", err)
+			return
+		}
+		defer file.Close() // Ensure the file is closed properly
+
+		// Write the content to the file with a newline at the end
+		if _, err := file.WriteString(plain + "\n"); err != nil {
+			fmt.Println("Error writing to file:", err)
+			return
+		}
 
 	}
 }
 
+func (l *Logger) ReplaceVars(data string, ps []interface{}) string {
+	for _, p := range ps {
+
+		data = l.VarsRepRegex.ReplaceAllString(data, fmt.Sprintf("%s", p))
+	}
+	return data
+}
+
 func GetCommandLine() string {
 	return fmt.Sprintf("%s %s", os.Args[0], filepath.Join(os.Args[1:]...))
+}
+
+func fileExists(filePath *string) bool {
+	if filePath == nil || *filePath == "" {
+		fmt.Println("Invalid or nil file path.")
+		return false
+	}
+
+	_, err := os.Stat(*filePath)
+	if os.IsNotExist(err) {
+		return false // File does not exist
+	}
+	return err == nil // File exists and no other error occurred
 }
